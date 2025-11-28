92102e93c18ab4a7e7ccd87e341921b4562cb556
Erik Saule <esaule@charlotte.edu>
2025-11-27 19:32:17 -0500

    eng

--- viewlog/opengl_framework.cpp
+++ viewlog/opengl_framework.cpp
@@ -2,37 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <iostream>
-
-void glCheckError() {
-    // Check for errors immediately after the draw call
-    GLenum error = glGetError(); 
-    
-    if (error != GL_NO_ERROR) {
-      // An error occurred!
-      std::cerr << "OpenGL Error after glDrawArrays: " << error << std::endl;
-      // You can convert the error code (like 1282) to its symbolic name (GL_INVALID_OPERATION)
-      // using a lookup table or debugging utility.
-    }
-}
-
-
-
-int  checkShaderError(GLuint vs) { 
-  // After glCompileShader check for errors.
-  int params = -1;
-  glGetShaderiv( vs, GL_COMPILE_STATUS, &params );
-  
-  // On error, capture the log and print it.
-  if ( GL_TRUE != params ) {
-    int max_length = 2048;
-    int actual_length = 0;
-    char slog[2048];
-    glGetShaderInfoLog( vs, max_length, &actual_length, slog );
-    fprintf( stderr, "ERROR: Shader index %u did not compile.\n%s\n", vs, slog );
-    return 1;
-  }
-  return 0;
-}
+#include "myglutils.hpp"
 
 GLuint getVertexShader() {
   const char* vertex_shader =
